79837ae03800fa7cc74af468ea3267ab22e26705
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 00:39:48 +0900

    삭제되지 않은 계좌 중 계좌번호와 일치하는 것 찾는 기능 추가

--- src/main/java/com/jh/accountmanagement/account/repository/AccountRepository.java
+++ src/main/java/com/jh/accountmanagement/account/repository/AccountRepository.java
@@ -4,6 +4,10 @@ import com.jh.accountmanagement.account.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumAndDelDate(long accountNum, LocalDateTime delDate);
 }
