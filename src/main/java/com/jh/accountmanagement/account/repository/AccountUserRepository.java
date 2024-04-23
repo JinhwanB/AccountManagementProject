e18c41203e30d5e26663dbbfead6aa214c08ea26
jinhwan Bea <paewhal20@gmail.com>
2024-04-23 13:27:08 +0900

    userId로 찾기 메소드 변경

--- src/main/java/com/jh/accountmanagement/account/repository/AccountUserRepository.java
+++ src/main/java/com/jh/accountmanagement/account/repository/AccountUserRepository.java
@@ -3,8 +3,9 @@ package com.jh.accountmanagement.account.repository;
 import com.jh.accountmanagement.account.model.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface AccountUserRepository extends JpaRepository<AccountUser, Long> {
-    Optional<AccountUser> findByUserId(String userId);
+    Optional<AccountUser> findByUserIdAndDelDate(String userId, LocalDateTime delDate);
 }
