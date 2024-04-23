d587c28c323962e263892c1a9df3a6783b9bda29
jinhwan Bea <paewhal20@gmail.com>
2024-04-23 12:38:35 +0900

    계좌 dto 구조 변경

---
+++ src/main/java/com/jh/accountmanagement/account/dto/AccountCreateResponse.java
@@ -0,0 +1,28 @@
+package com.jh.accountmanagement.account.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class AccountCreateResponse {
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Request {
+        private String userId;
+        private long initMoney;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Response {
+        private String userId;
+        private long accountNum;
+        private LocalDateTime regDate;
+    }
+}
