f20d0652b52607251ba14c81defa8a61a71ecb26
jinhwan Bea <paewhal20@gmail.com>
2024-04-23 11:07:08 +0900

    Create AccountCreateResponse.java

---
+++ src/main/java/com/jh/accountmanagement/dto/response/AccountCreateResponse.java
@@ -0,0 +1,16 @@
+package com.jh.accountmanagement.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+@ToString
+public class AccountCreateResponse {
+    private String userId;
+    private long accountNum;
+    private LocalDateTime regDate;
+}
