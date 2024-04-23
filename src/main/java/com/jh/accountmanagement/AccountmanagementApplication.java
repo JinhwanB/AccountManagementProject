00f1cce6a4cdbb22b22c3a243e23bea538f34bb8
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 00:40:39 +0900

    초기 데이터

--- src/main/java/com/jh/accountmanagement/AccountmanagementApplication.java
+++ src/main/java/com/jh/accountmanagement/AccountmanagementApplication.java
@@ -1,13 +1,26 @@
 package com.jh.accountmanagement;
 
+import com.jh.accountmanagement.account.model.AccountUser;
+import com.jh.accountmanagement.account.repository.AccountUserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class AccountmanagementApplication {
+    private final AccountUserRepository accountUserRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(AccountmanagementApplication.class, args);
     }
 
+    @PostConstruct
+    void init() {
+        AccountUser test = AccountUser.builder()
+                .userId("test")
+                .build();
+        accountUserRepository.save(test);
+    }
 }
