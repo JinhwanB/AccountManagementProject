9178c4bf89b1a42d8281b136a8248d8cd641c113
jinhwan Bea <paewhal20@gmail.com>
2024-04-23 13:11:06 +0900

    userId를 통해 찾기 추가

--- src/main/java/com/jh/accountmanagement/account/repository/AccountUserRepository.java
+++ src/main/java/com/jh/accountmanagement/account/repository/AccountUserRepository.java
@@ -3,5 +3,8 @@ package com.jh.accountmanagement.account.repository;
 import com.jh.accountmanagement.account.model.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountUserRepository extends JpaRepository<AccountUser, Long> {
+    Optional<AccountUser> findByUserId(String userId);
 }
