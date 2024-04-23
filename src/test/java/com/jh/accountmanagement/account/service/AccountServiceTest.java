e4ed245bf598c5c5e425734905f77f400e1a0211
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 00:41:44 +0900

    계좌 생성 테스트

--- src/test/java/com/jh/accountmanagement/account/service/AccountServiceTest.java
+++ src/test/java/com/jh/accountmanagement/account/service/AccountServiceTest.java
@@ -1,15 +1,13 @@
 package com.jh.accountmanagement.account.service;
 
 import com.jh.accountmanagement.account.dto.AccountCreate;
-import com.jh.accountmanagement.account.exception.NotFoundUserIdException;
-import com.jh.accountmanagement.account.model.AccountUser;
-import com.jh.accountmanagement.account.repository.AccountRepository;
+import com.jh.accountmanagement.account.model.Account;
 import com.jh.accountmanagement.account.repository.AccountUserRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,32 +19,33 @@ class AccountServiceTest {
     AccountService accountService;
 
     @Autowired
-    AccountRepository accountRepository;
-
-    @Autowired
     AccountUserRepository accountUserRepository;
 
     @Test
     @DisplayName("계좌 생성 테스트")
     void createAccount() {
         // given
-        AccountUser entity = AccountUser.builder()
-                .userId("test")
-                .build();
-        accountUserRepository.save(entity);
-
         AccountCreate.Request request = AccountCreate.Request.builder()
                 .initMoney(1000L)
                 .userId("test")
                 .build();
+        AccountCreate.Request request2 = AccountCreate.Request.builder()
+                .initMoney(10000L)
+                .userId("test")
+                .build();
 
         //when
-        accountService.createAccount(request);
+        Account account = accountService.createAccount(request);
 
         // then
-        AccountUser accountUser = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 유저는 없습니다."));
-        assertThat(accountUser.getUserId()).isEqualTo("test");
-        assertThat(accountUser.getAccountList()).hasSize(1);
-        assertThat(accountUser.getAccountList().get(0).getMoney()).isEqualTo(1000L);
+        assertThat(account.getAccountUser().getUserId()).isEqualTo("test");
+        assertThat(account.getAccountUser().getAccountList()).hasSize(1);
+        assertThat(account.getAccountUser().getAccountList().get(0).getMoney()).isEqualTo(1000L);
+
+        Account account2 = accountService.createAccount(request2);
+
+        assertThat(account2.getAccountUser().getUserId()).isEqualTo("test");
+        assertThat(account2.getAccountUser().getAccountList()).hasSize(2);
+        assertThat(account2.getAccountUser().getAccountList().get(1).getMoney()).isEqualTo(10000L);
     }
 }
