7715d3930fed1835c54da35f9969632bfefe4c3d
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 20:53:31 +0900

    exception 테스트 추가

--- src/test/java/com/jh/accountmanagement/account/repository/AccountRepositoryTest.java
+++ src/test/java/com/jh/accountmanagement/account/repository/AccountRepositoryTest.java
@@ -2,9 +2,7 @@ package com.jh.accountmanagement.account.repository;
 
 import com.jh.accountmanagement.account.domain.Account;
 import com.jh.accountmanagement.account.domain.AccountUser;
-import com.jh.accountmanagement.account.exception.AlreadyDeletedAccountException;
-import com.jh.accountmanagement.account.exception.NotFoundAccountException;
-import com.jh.accountmanagement.account.exception.NotFoundUserIdException;
+import com.jh.accountmanagement.account.exception.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -96,5 +94,77 @@ class AccountRepositoryTest {
         List<Account> accountList = accountRepository.findAllByAccountUserAndDelDate(accountUser, null);
         assertThat(accountList).hasSize(1);
         assertThat(accountList.get(0).getAccountNum()).isEqualTo(1234567893);
+    }
+
+    @Test
+    @DisplayName("계좌 10개 이상인 경우 exception")
+    void accountMaximumException() {
+        AccountUser accountUser = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 유저는 없습니다."));
+        long accountNum = 1234567893;
+        for (int i = 0; i < 10; i++) {
+            Account accountBuild = Account.builder()
+                    .accountNum(accountNum++)
+                    .accountUser(accountUser)
+                    .money(1000)
+                    .build();
+            accountBuild.setRegDate(LocalDateTime.now());
+            accountBuild.setChgDate(LocalDateTime.now());
+            accountRepository.save(accountBuild);
+        }
+        List<Account> accountList = accountRepository.findAllByAccountUserAndDelDate(accountUser, null);
+        try {
+            if (accountList.size() == 10) {
+                throw new AccountMaximumException("계좌는 최대 10개까지만 만들 수 있습니다.");
+            }
+        } catch (AccountMaximumException e) {
+            assertThat(e.getMessage()).isEqualTo("계좌는 최대 10개까지만 만들 수 있습니다.");
+        }
+    }
+
+    @Test
+    @DisplayName("계좌가 해지상태인 경우 Exception")
+    void AlreadyDeleteAccount() {
+        AccountUser accountUser = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 유저는 없습니다."));
+        Account accountBuild = Account.builder()
+                .accountNum(1234567893)
+                .accountUser(accountUser)
+                .money(1000)
+                .delDate(LocalDateTime.now())
+                .build();
+        accountBuild.setRegDate(LocalDateTime.now());
+        accountBuild.setChgDate(LocalDateTime.now());
+        accountRepository.save(accountBuild);
+
+        try {
+            Account account = accountRepository.findByAccountNum(1234567893).orElseThrow(() -> new NotFoundAccountException("해당 계좌번호와 일치하는 계좌가 없습니다."));
+            if (account.getDelDate() != null) {
+                throw new AlreadyDeletedAccountException("이미 해지된 계좌입니다.");
+            }
+        } catch (AlreadyDeletedAccountException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 해지된 계좌입니다.");
+        }
+    }
+
+    @Test
+    @DisplayName("잔액이 남아있을 때 해지 시 exception")
+    void FailDeleteAccount() {
+        AccountUser accountUser = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 유저는 없습니다."));
+        Account accountBuild = Account.builder()
+                .accountNum(1234567893)
+                .accountUser(accountUser)
+                .money(1000)
+                .build();
+        accountBuild.setRegDate(LocalDateTime.now());
+        accountBuild.setChgDate(LocalDateTime.now());
+        accountRepository.save(accountBuild);
+
+        try {
+            Account account = accountRepository.findByAccountNum(1234567893).orElseThrow(() -> new NotFoundAccountException("해당 계좌번호와 일치하는 계좌가 없습니다."));
+            if (account.getMoney() != 0) {
+                throw new DeleteAccountFailException("계좌에 잔액이 남아있어 해지하지 못합니다.");
+            }
+        } catch (DeleteAccountFailException e) {
+            assertThat(e.getMessage()).isEqualTo("계좌에 잔액이 남아있어 해지하지 못합니다.");
+        }
     }
 }
--- src/test/java/com/jh/accountmanagement/account/repository/AccountUserRepositoryTest.java
+++ src/test/java/com/jh/accountmanagement/account/repository/AccountUserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.jh.accountmanagement.account.repository;
 
+import com.jh.accountmanagement.account.domain.Account;
 import com.jh.accountmanagement.account.domain.AccountUser;
+import com.jh.accountmanagement.account.exception.NotFoundAccountException;
 import com.jh.accountmanagement.account.exception.NotFoundUserIdException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,11 +13,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class AccountUserRepositoryTest {
     @Autowired
     private AccountUserRepository accountUserRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @BeforeEach
     void before() {
@@ -33,5 +39,32 @@ class AccountUserRepositoryTest {
         AccountUser user = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 아이디의 유저는 없습니다."));
 
         assertThat(user.getUserId()).isEqualTo("test");
+    }
+
+    @Test
+    @DisplayName("사용자 찾기 실패")
+    void failAccountUserId() {
+        NotFoundUserIdException exception = assertThrows(NotFoundUserIdException.class, () -> accountUserRepository.findByUserIdAndDelDate("toast", null).orElseThrow(() -> new NotFoundUserIdException("해당 유저는 없습니다.")));
+        assertThat(exception.getMessage()).isEqualTo("해당 유저는 없습니다.");
+    }
+
+    @Test
+    @DisplayName("사용자와 계좌번호 불일치")
+    void diffAccountUserAndAccountNum() {
+        AccountUser user = accountUserRepository.findByUserIdAndDelDate("test", null).orElseThrow(() -> new NotFoundUserIdException("해당 아이디의 유저는 없습니다."));
+        Account account = Account.builder()
+                .accountNum(3249587201L)
+                .accountUser(user)
+                .money(30000)
+                .build();
+        account.setRegDate(LocalDateTime.now());
+        account.setChgDate(LocalDateTime.now());
+        accountRepository.save(account);
+
+        try {
+            accountRepository.findByAccountUserAndAccountNum(user, 3234586593L).orElseThrow(() -> new NotFoundAccountException("해당 계좌번호는 사용자의 계좌번호가 아닙니다."));
+        } catch (NotFoundAccountException e) {
+            assertThat(e.getMessage()).isEqualTo("해당 계좌번호는 사용자의 계좌번호가 아닙니다.");
+        }
     }
 }
