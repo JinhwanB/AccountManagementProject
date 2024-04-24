4c13b6cbf61c3ba7125e878dff379a3e878d0fe7
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 21:23:20 +0900

    계좌 확인 시 response로 변경 메소드 추가

--- src/main/java/com/jh/accountmanagement/account/domain/Account.java
+++ src/main/java/com/jh/accountmanagement/account/domain/Account.java
@@ -1,5 +1,6 @@
 package com.jh.accountmanagement.account.domain;
 
+import com.jh.accountmanagement.account.dto.AccountCheck;
 import com.jh.accountmanagement.account.dto.AccountCreate;
 import com.jh.accountmanagement.account.dto.AccountDelete;
 import com.jh.accountmanagement.global.BaseTimeEntity;
@@ -33,6 +34,7 @@ public class Account extends BaseTimeEntity {
     @Column
     private LocalDateTime delDate;
 
+    // 계좌 생성시 Response로 변경 메소드
     public AccountCreate.Response toCreateResponse() {
         return AccountCreate.Response.builder()
                 .accountNum(this.accountNum)
@@ -41,11 +43,20 @@ public class Account extends BaseTimeEntity {
                 .build();
     }
 
+    // 계좌 해지시 Response로 변경 메소드
     public AccountDelete.Response toDeleteResponse() {
         return AccountDelete.Response.builder()
                 .accountNum(this.accountNum)
                 .userId(this.accountUser.getUserId())
                 .delDate(this.getDelDate())
+                .build();
+    }
+
+    // 계좌 확인시 Response로 변경 메소드
+    public AccountCheck.Response toCheckResponse() {
+        return AccountCheck.Response.builder()
+                .accountNum(this.accountNum)
+                .money(this.money)
                 .build();
     }
 }
