afe7303051124b9adaba26601bcedd90dc91a472
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 22:40:39 +0900

    거래 타입 및 거래 결과 enum 메시지 확인 가능하도록 수정

--- src/main/java/com/jh/accountmanagement/transaction/type/TransactionResult.java
+++ src/main/java/com/jh/accountmanagement/transaction/type/TransactionResult.java
@@ -1,5 +1,13 @@
 package com.jh.accountmanagement.transaction.type;
 
+import lombok.Getter;
+
+@Getter
 public enum TransactionResult {
-    S, F
+    S("성공"), F("실패");
+    private final String message;
+
+    TransactionResult(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/com/jh/accountmanagement/transaction/type/TransactionType.java
+++ src/main/java/com/jh/accountmanagement/transaction/type/TransactionType.java
@@ -1,5 +1,13 @@
 package com.jh.accountmanagement.transaction.type;
 
+import lombok.Getter;
+
+@Getter
 public enum TransactionType {
-    TRANSACTION, CANCEL
+    TRANSACTION("잔액 사용"), CANCEL("잔액 사용 취소");
+    private final String message;
+
+    TransactionType(String message) {
+        this.message = message;
+    }
 }
