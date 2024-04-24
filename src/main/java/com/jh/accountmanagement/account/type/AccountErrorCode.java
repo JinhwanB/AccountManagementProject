49b8c3be3069ce9e93232a274385946a4ce55b7f
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 22:39:17 +0900

    이름 변경

--- src/main/java/com/jh/accountmanagement/account/type/AccountErrorCode.java
+++ src/main/java/com/jh/accountmanagement/account/type/AccountErrorCode.java
@@ -1,6 +1,9 @@
 package com.jh.accountmanagement.account.type;
 
-public enum ErrorCode {
+import lombok.Getter;
+
+@Getter
+public enum AccountErrorCode {
     NOT_FOUNT_USER_ID("해당 아이디의 사용자를 찾을 수 없습니다."),
     ACCOUNT_MAXIMUM("계좌는 최대 10개까지 만들 수 있습니다."),
     ALREADY_DELETED_ACCOUNT("이미 해지된 계좌입니다."),
@@ -8,7 +11,7 @@ public enum ErrorCode {
     NOT_FOUND_ACCOUNT("해당 계좌번호는 없는 번호입니다.");
     private final String message;
 
-    ErrorCode(String message) {
+    AccountErrorCode(String message) {
         this.message = message;
     }
 }
