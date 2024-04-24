43d3e1d17986a6fca047456508e9e1a9fe0d4f2b
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 14:06:41 +0900

    계좌 리스트 가져오는 쿼리 메소드 추가

--- src/main/java/com/jh/accountmanagement/account/repository/AccountRepository.java
+++ src/main/java/com/jh/accountmanagement/account/repository/AccountRepository.java
@@ -1,13 +1,17 @@
 package com.jh.accountmanagement.account.repository;
 
 import com.jh.accountmanagement.account.model.Account;
+import com.jh.accountmanagement.account.model.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findAllByAccountUserAndDelDate(AccountUser accountUser, LocalDateTime delDate);
+
     Optional<Account> findByAccountNumAndDelDate(long accountNum, LocalDateTime delDate);
 }
