cbde7b3354f904ed67bc7346170d30bcd1dbc9ae
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 14:06:22 +0900

    필드에 리스트 제거

--- src/main/java/com/jh/accountmanagement/account/model/AccountUser.java
+++ src/main/java/com/jh/accountmanagement/account/model/AccountUser.java
@@ -5,23 +5,17 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder(toBuilder = true)
-@ToString(exclude = "accountList")
+@ToString()
 public class AccountUser extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @OneToMany(mappedBy = "accountUser", cascade = CascadeType.ALL)
-    @Builder.Default
-    private List<Account> accountList = new ArrayList<>();
 
     @Column(nullable = false)
     private String userId;
