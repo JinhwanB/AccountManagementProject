a9b9e813e84dba77354abb1b7269fc5126cb4d43
jinhwan Bea <paewhal20@gmail.com>
2024-04-24 21:41:30 +0900

    계좌 확인 컨트롤러 추가

--- src/main/java/com/jh/accountmanagement/account/controller/AccountController.java
+++ src/main/java/com/jh/accountmanagement/account/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.jh.accountmanagement.account.controller;
 
 import com.jh.accountmanagement.account.domain.Account;
+import com.jh.accountmanagement.account.dto.AccountCheck;
 import com.jh.accountmanagement.account.dto.AccountCreate;
 import com.jh.accountmanagement.account.dto.AccountDelete;
 import com.jh.accountmanagement.account.service.AccountService;
@@ -10,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -17,15 +20,24 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
     private final AccountService accountService;
 
+    // 계좌 생성 컨트롤러
     @PostMapping("/account")
     public ResponseEntity<AccountCreate.Response> createAccount(@Valid @RequestBody AccountCreate.Request request) {
         Account account = accountService.createAccount(request);
         return ResponseEntity.ok(account.toCreateResponse());
     }
 
+    // 계좌 해지 컨트롤러
     @DeleteMapping("/account")
     public ResponseEntity<AccountDelete.Response> deleteAccount(@Valid @RequestBody AccountDelete.Request request) {
         Account account = accountService.deleteAccount(request);
         return ResponseEntity.ok(account.toDeleteResponse());
+    }
+
+    // 계좌 확인 컨트롤러
+    @GetMapping
+    public ResponseEntity<List<AccountCheck.Response>> checkAccount(@Valid @RequestBody AccountCheck.Request request) {
+        List<Account> accounts = accountService.checkAccount(request);
+        return ResponseEntity.ok(accounts.stream().map(Account::toCheckResponse).toList());
     }
 }
