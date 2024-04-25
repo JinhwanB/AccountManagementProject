8931452b74e106e890d360665cdb35aaf15b29dd
jinhwan Bea <paewhal20@gmail.com>
2024-04-25 10:39:50 +0900

    user를 통해 거래 리스트 조회 추가

--- src/main/java/com/jh/accountmanagement/transaction/repository/TransactionRepository.java
+++ src/main/java/com/jh/accountmanagement/transaction/repository/TransactionRepository.java
@@ -1,12 +1,16 @@
 package com.jh.accountmanagement.transaction.repository;
 
+import com.jh.accountmanagement.account.domain.AccountUser;
 import com.jh.accountmanagement.transaction.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     Optional<Transaction> findByTransactionNumber(String transactionNumber);
+
+    List<Transaction> findByAccountUser(AccountUser accountUser);
 }
