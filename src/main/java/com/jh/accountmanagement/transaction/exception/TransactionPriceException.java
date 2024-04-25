d5cd9c875cccba675538d6b9a921a7fc7909ca6e
jinhwan Bea <paewhal20@gmail.com>
2024-04-25 09:44:46 +0900

    거래 시 가격 관련 exception

---
+++ src/main/java/com/jh/accountmanagement/transaction/exception/TransactionPriceException.java
@@ -0,0 +1,11 @@
+package com.jh.accountmanagement.transaction.exception;
+
+public class TransactionPriceException extends RuntimeException {
+    public TransactionPriceException() {
+        super();
+    }
+
+    public TransactionPriceException(String message) {
+        super(message);
+    }
+}
