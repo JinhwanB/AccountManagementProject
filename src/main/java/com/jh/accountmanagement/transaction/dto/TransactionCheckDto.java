448050392e66436aa61bef772fa46ea73d2f0cd3
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-04-28 03:51:21 +0900

    거래 확인 dto 생성

---
+++ src/main/java/com/jh/accountmanagement/transaction/dto/TransactionCheckDto.java
@@ -0,0 +1,36 @@
+package com.jh.accountmanagement.transaction.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+public class TransactionCheckDto {
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Request {
+        @NotNull
+        @NotBlank
+        private String transactionNumber;
+    }
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    @ToString
+    public static class Response {
+        private long accountNum;
+        private String transactionType;
+        private String transactionResult;
+        private String transactionNumber;
+        private long transactionPrice;
+        private LocalDateTime transactionDate;
+    }
+}
