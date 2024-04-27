b4ba42fe14e6e6f1a419a0ef7bd6b64223a7913a
jinhwan Bea <paewhal20@gmail.com>
2024-04-27 20:53:56 +0900

    Update AccountDeleteDto.java

--- src/main/java/com/jh/accountmanagement/account/dto/AccountDeleteDto.java
+++ src/main/java/com/jh/accountmanagement/account/dto/AccountDeleteDto.java
@@ -1,6 +1,5 @@
 package com.jh.accountmanagement.account.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -22,7 +21,6 @@ public class AccountDeleteDto {
 
         @NotNull
         @Positive
-        @Min(10)
         private long accountNum;
     }
 
