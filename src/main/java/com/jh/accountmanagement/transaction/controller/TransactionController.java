390419289838314042ec41e9b48b5742a6b376e1
jinhwan Bea <paewhal20@gmail.com>
2024-04-27 21:31:45 +0900

    잔액 사용시 exception 처리 컨트롤러 추가

--- src/main/java/com/jh/accountmanagement/transaction/controller/TransactionController.java
+++ src/main/java/com/jh/accountmanagement/transaction/controller/TransactionController.java
@@ -2,9 +2,11 @@ package com.jh.accountmanagement.transaction.controller;
 
 import com.jh.accountmanagement.transaction.domain.Transaction;
 import com.jh.accountmanagement.transaction.dto.TransactionUseDto;
+import com.jh.accountmanagement.transaction.exception.TransactionPriceException;
 import com.jh.accountmanagement.transaction.service.TransactionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/transactions")
+@Slf4j
 public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/transaction")
     public ResponseEntity<TransactionUseDto.Response> useMoney(@Valid @RequestBody TransactionUseDto.Request request) {
-        Transaction transaction = transactionService.transactionUse(request);
+        Transaction transaction;
+        try {
+            transaction = transactionService.transactionUse(request);
+        } catch (TransactionPriceException e) {
+            log.error(e.getMessage());
+            transaction = transactionService.useFail(request);
+        }
         return ResponseEntity.ok(transaction.toUseResponse());
     }
 }
