22186b640c1e61ef52ec5bb3f4612a516059877e
jinhwan Bea <paewhal20@gmail.com>
2024-04-27 20:27:24 +0900

    잔액 사용 컨트롤러 추가

---
+++ src/main/java/com/jh/accountmanagement/transaction/controller/TransactionController.java
@@ -0,0 +1,23 @@
+package com.jh.accountmanagement.transaction.controller;
+
+import com.jh.accountmanagement.transaction.domain.Transaction;
+import com.jh.accountmanagement.transaction.dto.TransactionUseDto;
+import com.jh.accountmanagement.transaction.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public ResponseEntity<TransactionUseDto.Response> useMoney(@Valid @RequestBody TransactionUseDto.Request request) {
+        Transaction transaction = transactionService.transactionUse(request);
+        return ResponseEntity.ok(transaction.toUseResponse());
+    }
+}
