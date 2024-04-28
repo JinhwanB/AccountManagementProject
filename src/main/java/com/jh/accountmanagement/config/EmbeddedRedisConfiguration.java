6366ac1899320edf7afb3ba473d4f19015c5b0a3
jinhwan Bea <paewhal20@gmail.com>
2024-04-29 08:47:17 +0900

    Update EmbeddedRedisConfiguration.java

--- src/main/java/com/jh/accountmanagement/config/EmbeddedRedisConfiguration.java
+++ src/main/java/com/jh/accountmanagement/config/EmbeddedRedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.jh.accountmanagement.config;
 
+import io.micrometer.common.util.StringUtils;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
@@ -8,8 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 @Profile("local")
@@ -45,6 +48,65 @@ public class EmbeddedRedisConfiguration {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String OS = System.getProperty("os.name").toLowerCase();
+        System.out.println("OS: " + OS);
+        System.out.println(System.getProperty("os.arch"));
+        if (OS.contains("win")) {
+            log.info("OS is  " + OS + " " + port);
+            String command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port);
+            String[] shell = {"cmd.exe", "/y", "/c", command};
+            return Runtime.getRuntime().exec(shell);
+        }
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return !StringUtils.isEmpty(pidInfo.toString());
     }
 
     private boolean isArmArchitecture() {
