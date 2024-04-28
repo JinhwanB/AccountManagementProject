8bc7ab348f611a1bedd3f4a6997aaeecd74e87ef
jinhwan Bea <paewhal20@gmail.com>
2024-04-28 23:07:04 +0900

    삭제 추가

--- src/main/java/com/jh/accountmanagement/config/RedisUtils.java
+++ src/main/java/com/jh/accountmanagement/config/RedisUtils.java
@@ -21,6 +21,10 @@ public class RedisUtils {
         return redisTemplate.opsForValue().get(key);
     }
 
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
     public boolean hasKey(String key) {
         return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
