c14cb1b5cb6603e2d0cd6923663fbb2a29a8586a
jinhwan Bea <paewhal20@gmail.com>
2024-04-28 23:07:07 +0900

    Update AccountErrorCode.java

--- src/main/java/com/jh/accountmanagement/account/type/AccountErrorCode.java
+++ src/main/java/com/jh/accountmanagement/account/type/AccountErrorCode.java
@@ -8,7 +8,8 @@ public enum AccountErrorCode {
     ACCOUNT_MAXIMUM("계좌는 최대 10개까지 만들 수 있습니다."),
     ALREADY_DELETED_ACCOUNT("이미 해지된 계좌입니다."),
     DELETE_ACCOUNT_FAIL("해당 계좌에 잔액이 남아있어 해지할 수 없습니다."),
-    NOT_FOUND_ACCOUNT("해당 계좌번호는 없는 번호입니다.");
+    NOT_FOUND_ACCOUNT("해당 계좌번호는 없는 번호입니다."),
+    DIFF_USER_AND_ACCOUNT_NUMBER("해당 사용자의 계좌번호가 아닙니다.");
     private final String message;
 
     AccountErrorCode(String message) {
