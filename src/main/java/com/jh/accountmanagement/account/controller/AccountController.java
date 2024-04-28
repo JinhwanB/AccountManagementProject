9908c3e4645604cbf15f3e03c55de3a1d32df455
jinhwan Bea <paewhal20@gmail.com>
2024-04-29 03:22:44 +0900

    Update AccountController.java

--- src/main/java/com/jh/accountmanagement/account/controller/AccountController.java
+++ src/main/java/com/jh/accountmanagement/account/controller/AccountController.java
@@ -4,7 +4,9 @@ import com.jh.accountmanagement.account.domain.Account;
 import com.jh.accountmanagement.account.dto.AccountCheckDto;
 import com.jh.accountmanagement.account.dto.AccountCreateDto;
 import com.jh.accountmanagement.account.dto.AccountDeleteDto;
+import com.jh.accountmanagement.account.dto.AccountRedisDto;
 import com.jh.accountmanagement.account.service.AccountService;
+import com.jh.accountmanagement.config.RedisUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.List;
 @RequestMapping("/accounts")
 public class AccountController {
     private final AccountService accountService;
+    private final RedisUtils redisUtils;
 
     // 계좌 생성 컨트롤러
     @PostMapping("/account")
@@ -35,6 +38,11 @@ public class AccountController {
     // 계좌 확인 컨트롤러
     @GetMapping
     public ResponseEntity<List<AccountCheckDto.Response>> checkAccount(@Valid @RequestBody AccountCheckDto.Request request) {
+        if (redisUtils.hasKeyOfAccount(request.getUserId())) {
+            List<AccountRedisDto> accounts = redisUtils.getAccount(request.getUserId());
+            return ResponseEntity.ok(accounts.stream().map(AccountRedisDto::toCheckResponse).toList());
+        }
+
         List<Account> accounts = accountService.checkAccount(request);
         return ResponseEntity.ok(accounts.stream().map(Account::toCheckResponse).toList());
     }
