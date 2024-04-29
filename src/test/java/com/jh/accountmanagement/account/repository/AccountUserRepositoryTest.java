244108682c6f6d2ea346eb3a26b3e7a87b4a0984
jinhwan Bea <paewhal20@gmail.com>
2024-04-29 09:08:40 +0900

    Update AccountUserRepositoryTest.java

--- src/test/java/com/jh/accountmanagement/account/repository/AccountUserRepositoryTest.java
+++ src/test/java/com/jh/accountmanagement/account/repository/AccountUserRepositoryTest.java
@@ -4,7 +4,6 @@ import com.jh.accountmanagement.account.domain.Account;
 import com.jh.accountmanagement.account.domain.AccountUser;
 import com.jh.accountmanagement.account.exception.AccountException;
 import com.jh.accountmanagement.account.type.AccountErrorCode;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +21,6 @@ class AccountUserRepositoryTest {
 
     @Autowired
     private AccountRepository accountRepository;
-
-    @BeforeEach
-    void before() {
-        AccountUser accountUserBuild = AccountUser.builder()
-                .userId("test")
-                .build();
-        accountUserBuild.setRegDate(LocalDateTime.now());
-        accountUserBuild.setChgDate(LocalDateTime.now());
-        accountUserRepository.save(accountUserBuild);
-    }
 
     @Test
     @DisplayName("사용자 확인")
